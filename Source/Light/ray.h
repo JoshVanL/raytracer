57b00009026a942c669f9b360705b86d5d47ed94
JoshVanL <jv15626@my.bristol.ac.uk>
2018-02-21 01:04:49 +0000

    Adds kd-tree with fix

--- Source/Light/ray.h
+++ Source/Light/ray.h
@@ -29,12 +29,12 @@ public:
     bool ClosestIntersection(const std::vector<Shape2D*>& shapes, Intersection &closestIntersection,
                              const Shape2D* exclusion = nullptr) {
         closestIntersection.distance = std::numeric_limits<float>::max();
-        bool found = false;  
+        bool found = false;
 
         glm::vec4 intersectionPoint;
 
         for (size_t i=0; i<shapes.size(); i++) {
-            if((shapes[i]->intersect(*(this), (vec3) direction, 
+            if((shapes[i]->intersect(*(this), (vec3) direction,
                 intersectionPoint)) && (shapes[i] != exclusion)){
 
                 float d = glm::distance(intersectionPoint, position);
@@ -47,7 +47,6 @@ public:
 
                     found = true;
                 }
-                   
             }
         }
         return found;
@@ -56,4 +55,4 @@ public:
 };
 
 
-#endif 
+#endif
